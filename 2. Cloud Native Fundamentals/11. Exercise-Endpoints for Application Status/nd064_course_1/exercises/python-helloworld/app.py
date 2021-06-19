a30c3306587ad9ffe01abe4967d959b06b206007
Esua Ekpo <esuaekpo@yahoo.com>
2021-06-19 07:28:14 +0100

    implement endpoints for application status

--- 2. Cloud Native Fundamentals/11. Exercise-Endpoints for Application Status/nd064_course_1/exercises/python-helloworld/app.py
+++ 2. Cloud Native Fundamentals/11. Exercise-Endpoints for Application Status/nd064_course_1/exercises/python-helloworld/app.py
@@ -7,6 +7,34 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response = json.dumps({'result':'OK - Healthy'}),
+        status = 200,
+        mimetype = 'application/json'
+    )
+
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response = json.dumps(
+            {
+                "status":"sucess",
+                "code":0,
+                "data":{
+                    "UserCount":140,
+                    "UserCountActive":23
+                }
+            }
+        ),
+        status = 200,
+        mimetype = "application/json"
+    )
+
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
