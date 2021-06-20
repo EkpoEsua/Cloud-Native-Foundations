dc7bfec571f5a3a8224d1085b595211ee8731553
Esua Ekpo <esuaekpo@yahoo.com>
2021-06-20 07:59:14 +0100

    implement app logging functionality

--- 2. Cloud Native Fundamentals/Lesson 2 Architecture Consideration for Cloud Native Applications/11. Exercise-Endpoints for Application Status/nd064_course_1/exercises/python-helloworld/app.py
+++ 2. Cloud Native Fundamentals/Lesson 2 Architecture Consideration for Cloud Native Applications/11. Exercise-Endpoints for Application Status/nd064_course_1/exercises/python-helloworld/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from flask import json
+import logging
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+
+    #implement the logging functionality using Flask's inbuilt logger
+    app.logger.info('root endpoint was reached')
+
+    #implement the logger using python's default loggger
+    # logging.info("root endpoint was reached")
+
     return "Hello World!"
 
 @app.route("/status")
@@ -14,6 +23,12 @@ def status():
         status = 200,
         mimetype = 'application/json'
     )
+    
+    #implement the logging functionality using Flask's inbuilt logger
+    app.logger.info('status endpoint was reached')
+
+    #implement the logger using python's default loggger
+    # logging.info("status endpoint was reached")
 
     return response
 
@@ -33,8 +48,21 @@ def metrics():
         status = 200,
         mimetype = "application/json"
     )
+    
+    #implement the logging functionality using Flask's inbuilt logger
+    app.logger.info('metrics endpoint was reached')
+
+    #implement the logger using python's default loggger
+    # logging.info("metrics endpoint was reached")
 
     return response
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        filename='app.log',
+        level=logging.DEBUG,
+        style='{',
+        format='{asctime} , {message}'
+    )
+
     app.run(host='0.0.0.0')
